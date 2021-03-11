f582b0d4d664f0e6cbfc4990a5b62220b0fb20fc
becominginsane <francesco@zondax.ch>
2021-03-11 19:08:18 +0100

    fix(tee): various compilation errors

--- TEE/lib/src/optee.rs
+++ TEE/lib/src/optee.rs
@@ -1,4 +1,4 @@
-use zondee_utee::wrapper::{TEELogger, TaErrorCode as Error};
+use zondee_utee::wrapper::{raw::TEE_Param, TEELogger, TaErrorCode as Error};
 
 //The signatures of the following functions are defined in the framework's rustee_ta.h file
 
@@ -6,7 +6,7 @@ use zondee_utee::wrapper::{TEELogger, TaErrorCode as Error};
 pub extern "C" fn RUSTEE_Create() -> u32 {
     TEELogger::install().expect("unable to set logger");
     trace!("Creating");
-    
+
     /*
      *  Place your global initialization logic here
      * */
@@ -17,7 +17,7 @@ pub extern "C" fn RUSTEE_Create() -> u32 {
 #[no_mangle]
 pub extern "C" fn RUSTEE_Destroy() -> () {
     trace!("Destroying");
-    
+
     /*
      * Place your global cleanup logic here
      * */
@@ -30,7 +30,7 @@ pub extern "C" fn RUSTEE_OpenSession(
     _session_context: *const u8,
 ) -> u32 {
     trace!("Opening session");
-    
+
     /*
      * Place session initialization logic here
      */
@@ -41,7 +41,7 @@ pub extern "C" fn RUSTEE_OpenSession(
 #[no_mangle]
 pub extern "C" fn RUSTEE_CloseSession(_session_context: *const u8) -> () {
     trace!("Closing session");
-    
+
     /*
      * Place session cleanup logic here
      */
