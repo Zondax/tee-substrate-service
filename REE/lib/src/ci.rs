9688cd043d7b9195148eaa92e11240e65be6c4cb
becominginsane <francesco@zondax.ch>
2021-03-17 20:30:47 +0100

    refactor(ci): `Test` struct
    feat(ci): add new tests
    fix(ci): server address

--- REE/lib/src/ci.rs
+++ REE/lib/src/ci.rs
@@ -1,6 +1,8 @@
 use futures::future::Future;
 
+use futures::future::TryFutureExt;
 use jsonrpc_core_client::transports::http::connect;
+use schnorrkel::{PublicKey, Signature};
 use zkms_jsonrpc::ZKMSClient;
 
 pub async fn execute_tests(addr: impl std::net::ToSocketAddrs) {
@@ -16,16 +18,45 @@ pub async fn execute_tests(addr: impl std::net::ToSocketAddrs) {
     info!("TESTS STARTING");
 
     Test::new(
-        "generateNew",
+        "generateNew 00",
         "generate new keypair and return a public key; no seed",
         async {
-            match client.generate_new(None).await {
-                Err(e) => {
-                    error!("test failed with: {:?}", e);
-                    false
-                }
-                Ok(_) => true,
-            }
+            let key = client
+                .generate_new(None)
+                .await
+                .map_err(|e| format!("failed to issue request: {:?}", e))?;
+            PublicKey::from_bytes(&key[..])
+                .map_err(|e| format!("public key was not valid: {:?}", e))?; //verify that key is valid
+            Ok::<_, String>(())
+        },
+    )
+    .exec_fut()
+    .await;
+
+    Test::new(
+        "signMessage 00",
+        "sign a message and verify signature",
+        async {
+            let key = client
+                .generate_new(None)
+                .await
+                .map_err(|e| format!("failed to issue request: {:?}", e))?;
+            let public_key = PublicKey::from_bytes(&key[..])
+                .map_err(|e| format!("public key was not valid: {:?}", e))?; //verify that key is valid
+
+            const MSG: &[u8] = "francesco@zondax.ch".as_bytes();
+
+            let sign = client
+                .sign_message(key, MSG.to_vec())
+                .await
+                .map_err(|e| format!("failed to issue request: {:?}", e))?;
+
+            let sign = Signature::from_bytes(&sign[..])
+                .map_err(|e| format!("signature could not be deserialized: {:?}", e))?;
+
+            public_key
+                .verify_simple(b"zondax", MSG, &sign)
+                .map_err(|e| format!("signature was not valid: {:?}", e))
         },
     )
     .exec_fut()
@@ -35,7 +66,9 @@ pub async fn execute_tests(addr: impl std::net::ToSocketAddrs) {
 }
 
 async fn get_client(addr: std::net::SocketAddr) -> ZKMSClient {
-    connect::<ZKMSClient>(&addr.to_string())
+    let addr = format!("http://{}", addr);
+
+    connect::<ZKMSClient>(&addr)
         .await
         .expect("unable to connect to jsonrpc server")
 }
@@ -56,7 +89,11 @@ impl<'s, F> Test<'s, F> {
     }
 }
 
-impl<'s, F: Future<Output = bool>> Test<'s, F> {
+impl<'s, E, F> Test<'s, F>
+where
+    E: std::fmt::Debug,
+    F: Future<Output = Result<(), E>>,
+{
     #[allow(dead_code)]
     async fn exec_fut(self) -> bool {
         let Self {
@@ -68,17 +105,25 @@ impl<'s, F: Future<Output = bool>> Test<'s, F> {
         info!("[REMOTEE TEST {}]: START", name);
         debug!("[REMOTEE TEST {}]: {}", name, description);
         let result = logic.await;
-        info!(
-            "[REMOTEE TEST {}]: {}",
-            name,
-            if result { "SUCCESS" } else { "FAILURE" }
-        );
-
-        result
+        match result {
+            Ok(_) => {
+                info!("[REMOTEE TEST {}]: SUCCESS", name);
+                true
+            }
+            Err(e) => {
+                info!("[REMOTEE TEST {}]: FAILURE", name);
+                error!("[REMOTEE TEST {}]: REASON: {:?}", name, e);
+                false
+            }
+        }
     }
 }
 
-impl<'s, F: FnOnce() -> bool> Test<'s, F> {
+impl<'s, F, E> Test<'s, F>
+where
+    E: std::fmt::Debug,
+    F: FnOnce() -> Result<(), E>,
+{
     #[allow(dead_code)]
     fn exec(self) -> bool {
         let Self {
@@ -90,12 +135,16 @@ impl<'s, F: FnOnce() -> bool> Test<'s, F> {
         info!("[REMOTEE TEST {}]: START", name);
         debug!("[REMOTEE TEST {}]: {}", name, description);
         let result = (logic)();
-        info!(
-            "[REMOTEE TEST {}]: {}",
-            name,
-            if result { "SUCCESS" } else { "FAILURE" }
-        );
-
-        result
+        match result {
+            Ok(_) => {
+                info!("[REMOTEE TEST {}]: SUCCESS", name);
+                true
+            }
+            Err(e) => {
+                info!("[REMOTEE TEST {}]: FAILURE", name);
+                error!("[REMOTEE TEST {}]: REASON: {:?}", name, e);
+                false
+            }
+        }
     }
 }
